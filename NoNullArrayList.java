e1aed5d35dad82af289bb8e9817391136667a802
sheinamara <ashein00@stuy.edu>
2018-11-02 09:55:21 -0400

    everything compiled for nonullarraylist

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,10 +1,13 @@
 import java.util.ArrayList;
 public class NoNullArrayList<T> extends ArrayList<T>{
+  // CONSTRUCTORS
   public NoNullArrayList(){
   }
   public NoNullArrayList(int startingCapacity){
     super(startingCapacity);
   }
+
+  // SET METHOD
   public T set(int index, T element){
     if (element == null){
       throw new IllegalArgumentException("Cannot take null values!");
@@ -13,6 +16,8 @@ public class NoNullArrayList<T> extends ArrayList<T>{
       return super.set(index, element);
     }
   }
+
+  // ADD METHODS
   public void add(int index, T element){
     if (element == null){
       throw new IllegalArgumentException("Cannot take null values!");
@@ -21,7 +26,6 @@ public class NoNullArrayList<T> extends ArrayList<T>{
       super.add(index, element);
     }
   }
-  /*
   public boolean add(T element){
     if (element == null){
       throw new IllegalArgumentException("Cannot take null values!");
@@ -30,5 +34,5 @@ public class NoNullArrayList<T> extends ArrayList<T>{
       return super.add(element);
     }
   }
-  */
+
 }
